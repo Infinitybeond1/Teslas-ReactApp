dbf39795b4ffa274606c1da92ccc2b31392a7764
Infinitybeond1 <88919270+Infinitybeond1@users.noreply.github.com>
2021-11-20 11:25:56 -0600

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Automatically guided Vehicles</h1>
+      <h2>Driving into the future</h2>
+      <h3>What they do</h3>
+      <p1>
+        Self driving cars, AKA Automatically Guided Vehicles, are vehicles
+        (which are sometimes battery powered) that can drive themselves. These
+        types of vehicles use either Cameras, Radar, Lidar, Sonar, or a
+        combination of them. The most popular type of these cars is a tesla.{" "}
+      </p1>
+      <h3> Teslas </h3>
+      <p1>
+        Teslas are electric cars that use State of The Art technology to drive
+        themselves. They use a proprietary 8 camera setup which provide the car
+        with a 360° veiw of the car and its surroundings. Tesla models (2020 and
+        prior) are also equiped with radar technology, which are more acurate
+        than cameras but only up to 10 meters as the radio waves lose
+        effectiveness over a long distance.
+      </p1>
+      <h3>Cameras</h3>
+      <p1>The cameras on tesla cars have a max range of 250 meters</p1>
+    </div>
+  );
+}
